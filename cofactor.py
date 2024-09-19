a8e1d0c517e1c7164eabf9f3b09204b1bc350190
Carlos Yanez <carlitosyanez9@gmail.com>
2024-09-19 11:27:40 -0600

    correccion de matriz en suma y resta

--- cofactor.py
+++ cofactor.py
@@ -1,5 +1,4 @@
-# matriz.py
-
+# METODO COFACTORES
 def calcular_determinante_y_inversa(a):
     # Calcular determinante
     dP = a[0][0]*a[1][1]*a[2][2] + a[1][0]*a[2][1]*a[0][2] + a[2][0]*a[0][1]*a[1][2]
