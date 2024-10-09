f11ebe66f35ea1e2a9a5f0dfb492c6418a8a828c
carlos yanez <carlitosyanez9@gmail.com>
2024-10-08 21:18:10 -0600

    terminado

--- cofactor.py
+++ cofactor.py
@@ -7,12 +7,15 @@ def calcular_determinante_y_inversa(a):
         return None, "La matriz no tiene inversa (determinante = 0)"
 
     aCof = [[0]*3 for _ in range(3)]
+    #primera fila
     aCof[0][0] = a[1][1]*a[2][2] - a[1][2]*a[2][1]
     aCof[0][1] = -(a[1][0]*a[2][2] - a[2][0]*a[1][2])
     aCof[0][2] = a[1][0]*a[2][1] - a[1][1]*a[2][0]
+    #segunda fila
     aCof[1][0] = -(a[0][1]*a[2][2] - a[2][1]*a[0][2])
     aCof[1][1] = a[0][0]*a[2][2] - a[2][0]*a[0][2]
     aCof[1][2] = -(a[0][0]*a[2][1] - a[2][0]*a[0][1])
+    #tercera fila
     aCof[2][0] = a[0][1]*a[1][2] - a[1][1]*a[0][2]
     aCof[2][1] = -(a[0][0]*a[1][2] - a[1][0]*a[0][2])
     aCof[2][2] = a[0][0]*a[1][1] - a[1][0]*a[0][1]
